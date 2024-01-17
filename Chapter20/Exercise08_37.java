b1da62b47e8144dae7fa30283dd21b3d3fd6f77e
James <145038163+SteelSinter@users.noreply.github.com>
2024-01-17 09:17:24 -0700

    Add files via upload

--- Chapter20/Exercise08_37.java
+++ Chapter20/Exercise08_37.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Exercise08_37 {
   public static void main(String[] args) {
@@ -55,20 +55,22 @@ public class Exercise08_37 {
       {"Wyoming", "Cheyenne"}
     };
     
-    List list1 = Arrays.asList(stateCapital);
-    List<String> list = new LinkedList<String>(list1);
+    java.util.ArrayList<String[]> list = new java.util.ArrayList<String[]>(stateCapital.length);
+    
+    for (String[] s: stateCapital) {
+    	list.add(s);
+    }
 
     Scanner input = new Scanner(System.in);
     
     int correctCount = 0;
 
     for (int i = 0; i < stateCapital.length; i++) {
-    	int rand;
-    	do {
-    	  rand = (int)(Math.random() * 100);
-      } while (rand >= list.size());
-    	
-    	String[] item = (String[]) list.remove(rand);
+    	int rand = (((int)(Math.random() * 10)) * (list.size() - 1) / 9);
+    	System.out.println(rand);
+    	System.out.println(list.size());
+    	String[] item = list.remove(rand);
+    	list.trimToSize();
       // Prompt the user with a question
       System.out.print("What is the capital of " + item[0] + "? ");
       String capital = input.nextLine().trim().toLowerCase();
@@ -78,7 +80,7 @@ public class Exercise08_37 {
         correctCount++;
       }
       else
-        System.out.println("The correct answer should be " + stateCapital[i][1]);
+        System.out.println("The correct answer should be " + item[1]);
     }
 
     System.out.println("The correct count is " + correctCount);
