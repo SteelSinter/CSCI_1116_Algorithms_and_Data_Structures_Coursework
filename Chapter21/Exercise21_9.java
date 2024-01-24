dfbd43b02c2b8563af2d6256ce9c2388beb1269f
James <145038163+SteelSinter@users.noreply.github.com>
2024-01-24 12:58:16 -0700

    Add files via upload

---
+++ Chapter21/Exercise21_9.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class Exercise21_9 {
+  public static void main(String[] args) {
+    String[][] stateCapital = {
+      {"Alabama", "Montgomery"},
+      {"Alaska", "Juneau"},
+      {"Arizona", "Phoenix"},
+      {"Arkansas", "Little Rock"},
+      {"California", "Sacramento"},
+      {"Colorado", "Denver"},
+      {"Connecticut", "Hartford"},
+      {"Delaware", "Dover"},
+      {"Florida", "Tallahassee"},
+      {"Georgia", "Atlanta"},
+      {"Hawaii", "Honolulu"},
+      {"Idaho", "Boise"},
+      {"Illinois", "Springfield"},
+      {"Indiana", "Indianapolis"},
+      {"Iowa", "Des Moines"},
+      {"Kansas", "Topeka"},
+      {"Kentucky", "Frankfort"},
+      {"Louisiana", "Baton Rouge"},
+      {"Maine", "Augusta"},
+      {"Maryland", "Annapolis"},
+      {"Massachusettes", "Boston"},
+      {"Michigan", "Lansing"},
+      {"Minnesota", "Saint Paul"},
+      {"Mississippi", "Jackson"},
+      {"Missouri", "Jefferson City"},
+      {"Montana", "Helena"},
+      {"Nebraska", "Lincoln"},
+      {"Nevada", "Carson City"},
+      {"New Hampshire", "Concord"},
+      {"New Jersey", "Trenton"},
+      {"New York", "Albany"},
+      {"New Mexico", "Santa Fe"},
+      {"North Carolina", "Raleigh"},
+      {"North Dakota", "Bismarck"},
+      {"Ohio", "Columbus"},
+      {"Oklahoma", "Oklahoma City"},
+      {"Oregon", "Salem"},
+      {"Pennsylvania", "Harrisburg"},
+      {"Rhode Island", "Providence"},
+      {"South Carolina", "Columbia"},
+      {"South Dakota", "Pierre"},
+      {"Tennessee", "Nashville"},
+      {"Texas", "Austin"},
+      {"Utah", "Salt Lake City"},
+      {"Vermont", "Montpelier"},
+      {"Virginia", "Richmond"},
+      {"Washington", "Olympia"},
+      {"West Virginia", "Charleston"},
+      {"Wisconsin", "Madison"},
+      {"Wyoming", "Cheyenne"}
+    };
+    
+    Map<String, String> list = new HashMap<String, String>();
+    
+    for (String[] pair: stateCapital) {
+    	list.put(pair[0], pair[1]);
+    }
+    
+    System.out.println(list.toString());
+
+    Scanner input = new Scanner(System.in);
+    
+    String state;
+    
+    while (true) {
+    	System.out.println("Enter a state to get it's capital: ");
+    	state = input.nextLine();
+    	state = state.replace(state.charAt(0), Character.toUpperCase(state.charAt(0)));
+    	System.out.println("The capital of " + state + " is " + list.get(state));
+    	
+    }
+  }
+}
